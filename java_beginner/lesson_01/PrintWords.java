9ff5fee9114faef41ea9b49f6dbdbb0677aaaa43
Serge018 <sivenskij@gmail.com>
2021-11-03 23:20:23 +0300

    Добавлен класс PrintWords
    Реализован метод для вывода трёх слов в столбец. Слова - Orange, Banana, Apple

---
+++ java_beginner/lesson_01/PrintWords.java
@@ -0,0 +1,11 @@
+public class PrintWords {
+    public static void main(String[] args){
+        printThreeWords();
+    }
+
+    static void printThreeWords(){
+        String three_words = "Orange\n" + "Banana\n" + "Banana\n";
+
+        System.out.println(three_words);
+    }
+}
