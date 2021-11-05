fc55e2fe7c67346ea06fcca5f4ac52d232f1c664
Serge018 <sivenskij@gmail.com>
2021-11-05 18:02:25 +0300

    Создан класс Lesson02. Реаизован метод checkSummContainedInRange()

---
+++ java_beginner/lesson_02/Lesson02.java
@@ -0,0 +1,15 @@
+public class Lesson02{
+    public static void main(String[] args){
+        checkSummContainedInRange(8, 4);
+    }
+
+    public static boolean checkSummContainedInRange(int a, int b){
+        int rangeStart = 10;
+        int rangeEnd = 21;
+
+        int summ = a + b;
+        boolean isContain = (summ >= rangeStart && summ <= rangeEnd);
+
+        return isContain;
+    }
+}
