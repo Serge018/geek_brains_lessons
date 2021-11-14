50fc588884d7cfa2391cc3177b3aea0fe58f5232
Serge018 <sivenskij@gmail.com>
2021-11-14 17:04:19 +0300

    Реализовано решение задачи №1
    Добавлены методы getArrayRandomBinary() и setArrayRandomBinaryAlternativeValues()

--- java_beginner/lesson_03/Lesson03.java
+++ java_beginner/lesson_03/Lesson03.java
@@ -4,8 +4,44 @@
  * @author Sergey Ivenskiy
  * @version 14.10.2021
  */
+import java.util.Arrays;
+import java.util.Random;
+
+
 public class Lesson03 {
     public static void main(String[] args) {
+        // task 1
+        int[] arrayBinary = getArrayRandomBinary();
+        System.out.println(Arrays.toString(arrayBinary));
 
+        arrayBinary = setArrayRandomBinaryAlternativeValues(arrayBinary);
+        System.out.println(Arrays.toString(arrayBinary));
+    }
+
+
+    static int[] getArrayRandomBinary()
+    {
+        Random random = new Random();
+        int arrayLength = random.nextInt(10) + 1;
+        int[] arrayBinary = new int[arrayLength];
+
+        for (int iterate = 0; iterate < arrayLength; iterate++) {
+            arrayBinary[iterate] = random.nextInt(2);
+        }
+
+        return arrayBinary;
+    }
+
+
+    static int[] setArrayRandomBinaryAlternativeValues(int[] arrayBinary) {
+        int arrayLength = arrayBinary.length;
+
+        for (int iterate = 0; iterate < arrayLength; iterate++) {
+            int alternativeValue = (arrayBinary[iterate] == 0) ? 1 : 0;
+
+            arrayBinary[iterate] = alternativeValue;
+        }
+
+        return arrayBinary;
     }
 }
