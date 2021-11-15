a956c4f34b814d8e071a581b6a3e8a2d779eec69
Serge018 <sivenskij@gmail.com>
2021-11-15 21:58:11 +0300

    Добавлен класс TicTacToe

---
+++ java_beginner/lesson_04/TicTacToe.java
@@ -0,0 +1,20 @@
+/**
+ * Java 1. Homework 4
+ *
+ * @author Sergey Ivenskiy
+ * @version 15.10.2021
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        new TicTacToe();
+    }
+
+
+    TicTacToe() {
+        System.out.println("Hi!");
+    }
+}
