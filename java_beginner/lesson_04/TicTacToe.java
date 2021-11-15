0d3fea0c458e33cf531c619b0a7b39b036798dde
Serge018 <sivenskij@gmail.com>
2021-11-15 22:03:14 +0300

    Классу TicTacToe добавлены необходимые поля
    Создание объектов table, scanner и random перенесено в конструктор

--- java_beginner/lesson_04/TicTacToe.java
+++ java_beginner/lesson_04/TicTacToe.java
@@ -9,12 +9,29 @@ import java.util.Random;
 
 
 public class TicTacToe {
+    final int SIZE = 3;
+    final int WIN_SIZE = 3;
+    final char SIGN_X = 'x';
+    final char SIGN_O = 'o';
+    final char SIGN_EMPTY = '.';
+    final String MSG_FOR_HUMAN = "Enter X and Y [1..3]:";
+    final String MSG_YOU_WON = "You won!";
+    final String MSG_AI_WON = "AI won!";
+    final String MSG_DRAW = "Sorry, draw!";
+    final String MSG_GAME_OVER = "Game over.";
+    char[][] table;
+    Scanner scanner;
+    Random random;
+
+
     public static void main(String[] args) {
         new TicTacToe();
     }
 
 
     TicTacToe() {
-        System.out.println("Hi!");
+        table = new char[SIZE][SIZE];
+        scanner = new Scanner(System.in);
+        random = new Random();
     }
 }
