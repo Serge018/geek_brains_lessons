665aec7ba85e56736e56ea21abdd62fa3adb7b6b
Serge018 <sivenskij@gmail.com>
2021-11-21 22:30:49 +0300

    Добавлен класс Lesson05

---
+++ java_beginner/lesson_05/Lesson05.java
@@ -0,0 +1,13 @@
+/**
+ * Java 1. Homework 5
+ *
+ * @author Sergey Ivenskiy
+ * @version 21.11.2021
+ */
+public class Lesson05 {
+    public static void main(String[] args) {
+        Employee man = new Employee("Вася", "Дворник", "", "", 20000, 17);
+
+        System.out.println(man);
+    }
+}
