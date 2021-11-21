b28963d51f1ab59187594025d61b98e7918abaa1
Serge018 <sivenskij@gmail.com>
2021-11-21 22:46:24 +0300

    В метод main класса Lesson05 довлен массив экземляров класса Employee
    Добавлен вывод в консоль данных всех экземпляров класса Employee

--- java_beginner/lesson_05/Lesson05.java
+++ java_beginner/lesson_05/Lesson05.java
@@ -6,8 +6,16 @@
  */
 public class Lesson05 {
     public static void main(String[] args) {
-        Employee man = new Employee("Вася", "Дворник", "", "", 20000, 17);
+        Employee[] employees = {
+            new Employee("John Silver", "Tech-lead", "236-01-24", "admin@mail.ru", 550000, 42),
+            new Employee("Dart Vayder", "QA-lead", "544-13-43", "qa_support@mail.ru", 300000, 29),
+            new Employee("Vladilen Minin", "Frontend-lead", "234-54-25", "minin@mail.ru", 250000, 30),
+            new Employee("Ivan Ivanovich Ivanov", "Frontend Developer", "777-77-77", "ivanov1998@mail.ru", 130000, 25),
+            new Employee("Dart Vayder", "Backend Developer", "003-03-03", "darkside@mail.ru", 200000, 38)
+        };
 
-        System.out.println(man);
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
     }
 }
