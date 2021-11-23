913699af09cebdfae6f08bdcff30db9303e798f7
Serge018 <sivenskij@gmail.com>
2021-11-24 00:34:31 +0300

    Классу Animal добавлены статические методы приватные поля для подсчёта потомков
    Добавлены методы для инкремента полей подсчёта потомков

--- java_beginner/lesson_06/Animal.java
+++ java_beginner/lesson_06/Animal.java
@@ -5,6 +5,8 @@
  * @version 23.11.2021
  */
 public abstract class Animal implements IAnimal {
+    private static int childsCat = 0;
+    private static int childsDog = 0;
     protected String name;
     protected String color;
     protected int age;
@@ -14,6 +16,21 @@ public abstract class Animal implements IAnimal {
         this.name = name;
         this.color = color;
         this.age = age;
+    }
+
+
+    public static int incrementChildrenCat() {
+        return childsCat++;
+    }
+
+
+    public static int incrementChildrenDog() {
+        return childsDog++;
+    }
+
+
+    public static void printChildrensInfo() {
+        System.out.println("childs\nCats : " + childsCat + "\nDog : " + childsDog);
     }
 
 
