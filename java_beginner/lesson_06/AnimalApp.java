585bcbaf30e98e3cd85e84a8bb87161d49ba4d68
Serge018 <sivenskij@gmail.com>
2021-11-23 23:57:19 +0300

    Добавлен класс AnimalApp

---
+++ java_beginner/lesson_06/AnimalApp.java
@@ -0,0 +1,18 @@
+/**
+ * Java 1. Homework 5
+ *
+ * @author Sergey Ivenskiy
+ * @version 23.11.2021
+ */
+public class AnimalApp {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Tom", "black", 7);
+        Dog dog = new Dog("Spike", "grey", 8);
+
+        Animal[] animals = { cat, dog };
+        for (Animal animal : animals) {
+            System.out.println(animal);
+            System.out.println(animal.voice());
+        }
+    }
+}
