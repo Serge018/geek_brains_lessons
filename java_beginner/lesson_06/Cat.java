421d8fdc6c4f2f5b50bc70dc401d2dda37095bfa
Serge018 <sivenskij@gmail.com>
2021-11-24 00:00:35 +0300

    Добавлены классы Cat и Dog

---
+++ java_beginner/lesson_06/Cat.java
@@ -0,0 +1,17 @@
+/**
+ * Java 1. Homework 5
+ *
+ * @author Sergey Ivenskiy
+ * @version 23.11.2021
+ */
+public class Cat extends Animal {
+    Cat(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+
+    @Override
+    public String voice() {
+        return "mew";
+    }
+}
