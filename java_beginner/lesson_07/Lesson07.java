b40d90774297c7db5a693fd8b4c6a12d1285ff3a
Serge018 <sivenskij@gmail.com>
2021-11-29 00:14:41 +0300

    Homework 7. Создан массив котов. Кормление котов произовадится в двух итерациях

--- java_beginner/lesson_07/Lesson07.java
+++ java_beginner/lesson_07/Lesson07.java
@@ -6,12 +6,31 @@
  */
 public class Lesson07 {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 10);
-        Plate plate = new Plate(30);
+        Cat[] cats = {
+            new Cat("Barsik", 10),
+            new Cat("Bob", 35),
+            new Cat("Murzik", 3),
+            new Cat("Musya", 12)
+        };
 
-        cat.eat(plate);
+        Plate plate = new Plate(40);
 
-        System.out.println(cat);
-        System.out.println(plate);
+        // Feed the cats
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            // System.out.println(cat);
+            // System.out.println(plate);
+        }
+
+        // Refilling the plate with food
+        plate.increaseFood(40);
+        // System.out.println("\n");
+
+        // Feed the cats again
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            // System.out.println(cat);
+            // System.out.println(plate);
+        }
     }
 }
